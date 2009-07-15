f4828ed4a0206403d37c6ef78e693389e845b3df
Tim Blechmann <tim@klingt.org>
2009-07-15 17:48:38 +0200

    generic implementation of amplitude modulation routines
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- ampmod.hpp
+++ ampmod.hpp
@@ -1,6 +1,6 @@
 //  amplitude modulation
 //
-//  Copyright (C) 2008 Tim Blechmann
+//  Copyright (C) 2009 Tim Blechmann
 //
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
@@ -17,162 +17,13 @@
 //  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 //  Boston, MA 02111-1307, USA.
 
-#ifndef DSP_AMPMOD_HPP
-#define DSP_AMPMOD_HPP
+#ifndef SIMD_AMPMOD_HPP
+#define SIMD_AMPMOD_HPP
 
-#ifdef __SSE2__
-#include <xmmintrin.h>
-#include <emmintrin.h>
-#include "simd_utils.hpp"
+#include "ampmod_generic.hpp"
+
+#ifdef __SSE__
+#include "ampmod_sse.hpp"
 #endif
 
-namespace nova
-{
-
-template <typename float_type>
-inline void amp_mod(float_type * out, const float_type * in1, const float_type * in2,
-                    const float_type amount, unsigned int n)
-{
-    for (unsigned int i = 0; i != n; ++i)
-        out[i] = in1[i] * (float_type(1) + in2[i] * amount);
-}
-
-template <typename float_type>
-inline void amp_mod(float_type * out, const float_type * in1, const float_type * in2,
-                    float_type amount, const float_type amount_slope,
-                    unsigned int n)
-{
-    for (unsigned int i = 0; i != n; ++i)
-    {
-        out[i] = in1[i] * (float_type(1) + in2[i] * amount);
-        amount += amount_slope;
-    }
-}
-
-template <typename float_type>
-inline void amp_mod(float_type * out, const float_type * in1, const float_type * in2,
-                    const float_type * amount, unsigned int n)
-{
-    for (unsigned int i = 0; i != n; ++i)
-        out[i] = in1[i] * (float_type(1) + in2[i] * amount[i]);
-}
-
-template <typename float_type>
-inline void amp_mod4(float_type * out, const float_type * in1, const float_type * in2,
-                     const float_type amount, unsigned int n)
-{
-    amp_mod(out, in1, in2, amount, n);
-}
-
-template <typename float_type>
-inline void amp_mod4(float_type * out, const float_type * in1, const float_type * in2,
-                     float_type amount, const float_type amount_slope,
-                     unsigned int n)
-{
-    amp_mod4(out, in1, in2, amount, amount_slope, n);
-}
-
-template <typename float_type>
-inline void amp_mod4(float_type * out, const float_type * in1, const float_type * in2,
-                     const float_type * amount, unsigned int n)
-{
-    amp_mod(out, in1, in2, amount, n);
-}
-
-#ifdef __SSE2__
-namespace detail
-{
-
-/** compute x1 * (1 + x2 * amount)  */
-__m128 inline amp_mod4_loop(__m128 x1, __m128 x2, __m128 amount, __m128 one)
-{
-    return _mm_mul_ps(x1,
-                      _mm_add_ps(one,
-                                 _mm_mul_ps(x2, amount)));
-}
-
-} /* namespace detail */
-
-template <>
-inline void amp_mod4(float * out, const float * in1, const float * in2,
-                     const float amount, unsigned int n)
-{
-    std::size_t loops = n >> 2;
-    const __m128 one = detail::gen_one();
-
-    const __m128 amnt = _mm_set_ps1(amount);
-
-    do
-    {
-        const __m128 x1 = _mm_load_ps(in1);
-        in1 += 4;
-        const __m128 x2 = _mm_load_ps(in2);
-        in2 += 4;
-
-        const __m128 result = detail::amp_mod4_loop(x1, x2, amnt, one);
-
-        _mm_store_ps(out, result);
-        out += 4;
-    }
-    while (--loops);
-}
-
-template <>
-inline void amp_mod4(float * out, const float * in1, const float * in2,
-                     const float amount, const float amount_slope, unsigned int n)
-{
-    std::size_t loops = n >> 2;
-    const __m128 one = detail::gen_one();
-
-    __m128 amnt = _mm_set_ps(amount,
-                             amount + amount_slope,
-                             amount + amount_slope + amount_slope,
-                             amount + amount_slope + amount_slope + amount_slope);
-    const __m128 slope = _mm_set_ps1(4 * amount_slope);
-
-    do
-    {
-        const __m128 x1 = _mm_load_ps(in1);
-        in1 += 4;
-        const __m128 x2 = _mm_load_ps(in2);
-        in2 += 4;
-
-        const __m128 result = detail::amp_mod4_loop(x1, x2, amnt, one);
-
-        amnt = _mm_add_ps(amnt, slope);
-
-        _mm_store_ps(out, result);
-        out += 4;
-    }
-    while (--loops);
-}
-
-template <>
-inline void amp_mod4(float * out, const float * in1, const float * in2,
-                     const float * amount, unsigned int n)
-{
-    std::size_t loops = n >> 2;
-    const __m128 one = detail::gen_one();
-
-    do
-    {
-        const __m128 x1 = _mm_load_ps(in1);
-        in1 += 4;
-        const __m128 x2 = _mm_load_ps(in2);
-        in2 += 4;
-        const __m128 amnt = _mm_load_ps(amount);
-        amount += 4;
-
-        const __m128 result = detail::amp_mod4_loop(x1, x2, amnt, one);
-
-        _mm_store_ps(out, result);
-        out += 4;
-    }
-    while (--loops);
-}
-
-#endif /* __SSE2__ */
-
-} /* namespace nova */
-
-#endif /* DSP_AMPMOD_HPP */
+#endif /* SIMD_AMPMOD_HPP */
