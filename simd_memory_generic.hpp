70ab5e3786a17c4dbd480483100a422348c08634
Tim Blechmann <tim@klingt.org>
2009-07-16 17:59:16 +0200

    implemented addvec function
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- simd_memory_generic.hpp
+++ simd_memory_generic.hpp
@@ -153,6 +153,122 @@ inline void copyvec_na_simd(F * dest, const F * src, uint n)
     std::memcpy(dest, src, n*sizeof(F));
 }
 
+template <typename F>
+inline void addvec(F * out, const F * in, unsigned int n)
+{
+    do
+    {
+        *out++ += *in++;
+    }
+    while (--n);
+}
+
+template <typename F>
+inline void addvec(F * out, const F in, unsigned int n)
+{
+    do
+    {
+        *out++ += in;
+    }
+    while (--n);
+}
+
+template <typename F>
+inline void addvec(F * out, const F in, const F slope, unsigned int n)
+{
+    do
+    {
+        *out++ += in; in += slope;
+    }
+    while (--n);
+}
+
+template <typename F>
+inline void addvec_simd(F * out, const F * in, unsigned int n)
+{
+    do
+    {
+        F out0 = out[0] + in[0];
+        F out1 = out[1] + in[1];
+        F out2 = out[2] + in[2];
+        F out3 = out[3] + in[3];
+        F out4 = out[4] + in[4];
+        F out5 = out[5] + in[5];
+        F out6 = out[6] + in[6];
+        F out7 = out[7] + in[7];
+
+        out[0] = out0;
+        out[1] = out1;
+        out[2] = out2;
+        out[3] = out3;
+        out[4] = out4;
+        out[5] = out5;
+        out[6] = out6;
+        out[7] = out7;
+
+        out += 8;
+        in += 8;
+    }
+    while (--n);
+}
+
+template <typename F>
+inline void addvec_simd(F * out, const F in, unsigned int n)
+{
+    do
+    {
+        F out0 = out[0] + in;
+        F out1 = out[1] + in;
+        F out2 = out[2] + in;
+        F out3 = out[3] + in;
+        F out4 = out[4] + in;
+        F out5 = out[5] + in;
+        F out6 = out[6] + in;
+        F out7 = out[7] + in;
+
+        out[0] = out0;
+        out[1] = out1;
+        out[2] = out2;
+        out[3] = out3;
+        out[4] = out4;
+        out[5] = out5;
+        out[6] = out6;
+        out[7] = out7;
+
+        out += 8;
+    }
+    while (--n);
+}
+
+template <typename F>
+inline void addvec_simd(F * out, const F in, const F slope, unsigned int n)
+{
+    do
+    {
+        F out0 = out[0] + in; in += slope;
+        F out1 = out[1] + in; in += slope;
+        F out2 = out[2] + in; in += slope;
+        F out3 = out[3] + in; in += slope;
+        F out4 = out[4] + in; in += slope;
+        F out5 = out[5] + in; in += slope;
+        F out6 = out[6] + in; in += slope;
+        F out7 = out[7] + in; in += slope;
+
+        out[0] = out0;
+        out[1] = out1;
+        out[2] = out2;
+        out[3] = out3;
+        out[4] = out4;
+        out[5] = out5;
+        out[6] = out6;
+        out[7] = out7;
+
+        out += 8;
+    }
+    while (--n);
+}
+
+
 } /* namespace nova */
 
 #endif /* SIMD_MEMORY_GENERIC_HPP */
