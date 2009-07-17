0a8eec4074fc16074dcc2410f536106864a4429f
Tim Blechmann <tim@klingt.org>
2009-07-17 11:33:28 +0200

    implement compile-time unrolled addvec_simd functions
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- simd_memory.hpp
+++ simd_memory.hpp
@@ -68,6 +68,25 @@ inline void copyvec_na_simd_mp(double * dest, const double * src)
     copyvec_na_simd(dest, src, n);
 }
 
+
+template <unsigned int n>
+inline void addvec_simd(float * dest, const float * in)
+{
+    addvec_simd(dest, in, n);
+}
+
+template <unsigned int n>
+inline void addvec_simd(float * dest, const float in)
+{
+    addvec_simd(dest, in, n);
+}
+
+template <unsigned int n>
+inline void addvec_simd(float * dest, const float in, const float slope)
+{
+    addvec_simd(dest, in, slope, n);
+}
+
 } /* namespace nova */
 
 #endif /* SIMD_MEMORY_HPP */
