9cc16516018e45a54acaa6da3e7573a4e7480235
Tim Blechmann <tim@klingt.org>
2009-07-23 11:39:44 +0200

    testsuite: added tests for binary arithmetic functions
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- testsuite/test_helper.hpp
+++ testsuite/test_helper.hpp
@@ -3,15 +3,20 @@
 #include <boost/random.hpp>
 
 template <typename float_type>
-void randomize_buffer(float_type * buffer, std::size_t size)
+float_type randomize_float(void)
 {
     static boost::mt19937 rng;
     static boost::uniform_real<float_type> dist(-1, 1);
     static boost::variate_generator<boost::mt19937&, boost::uniform_real<float_type> >
         gen(rng, dist);
+    return gen();
+}
 
+template <typename float_type>
+void randomize_buffer(float_type * buffer, std::size_t size)
+{
     for (std::size_t i = 0; i != size; ++i)
-        buffer[i] = gen();
+        buffer[i] = randomize_float<float_type>();
 }
 
 template <typename float_type>
