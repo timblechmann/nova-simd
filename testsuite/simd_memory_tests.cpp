a7c8f7b677199249dec09e57678f39d0bd07377e
Tim Blechmann <tim@klingt.org>
2012-01-16 14:51:55 +0100

    testsuite: relax set_exp test
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- testsuite/simd_memory_tests.cpp
+++ testsuite/simd_memory_tests.cpp
@@ -117,7 +117,7 @@ void exp_tests(void)
     set_exp_vec<float_type>(out.c_array(), base, slope, size);
     set_exp_vec_simd<float_type>(out_simd.c_array(), base, slope, size);
 
-    compare_buffers(out.c_array(), out_simd.c_array(), size, 1e-6);
+    compare_buffers(out.c_array(), out_simd.c_array(), size, 1e-5);
 }
 
 BOOST_AUTO_TEST_CASE( exp_test )
